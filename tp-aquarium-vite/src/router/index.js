657fb17b60429aa61ccbbd61efb3e7b485945830
CharlietheCharlie <st896218@gmail.com>
2023-05-11 20:25:12 +0800

    5/11 subpage

--- tp-aquarium-vite/src/router/index.js
+++ tp-aquarium-vite/src/router/index.js
@@ -9,11 +9,7 @@ const router = createRouter({
       name: "index",
       component: index,
     },
-    {
-      path: "/yearpass",
-      name: "Yearpass",
-      component: () => import("../views/Yearpass.vue"),
-    },
+ 
     {
       path: "/cart",
       name: "Cart",
@@ -23,27 +19,33 @@ const router = createRouter({
       path: "/game_index",
       name: "GameIndex",
       component: () => import("../views/GameIndex.vue"),
+      children:[
+          {
+            path: "game_question",
+            name: "GameQuestion",
+            component: () => import("../views/game/GameQuestion.vue"),
+          },
+          {
+            path: "game_puzzle",
+            name: "GamePuzzle",
+            component: () => import("../views/game/GamePuzzle.vue"),
+          },
+        ]
     },
-    {
-      path: "/game_question",
-      name: "GameQuestion",
-      component: () => import("../views/GameQuestion.vue"),
-    },
-    {
-      path: "/game_puzzle",
-      name: "GamePuzzle",
-      component: () => import("../views/GamePuzzle.vue"),
-    },
+    
     {
       path: "/journey",
       name: "Journey",
       component: () => import("../views/Journey.vue"),
+      children:[
+        {
+          path: "overnight",
+          name: "Overnight",
+          component: () => import("../views/journey/Overnight.vue"),
+        },
+      ]
     },
-    {
-      path: "/overnight",
-      name: "Overnight",
-      component: () => import("../views/Overnight.vue"),
-    },
+   
     {
       path: "/faq",
       name: "Faq",
@@ -70,12 +72,12 @@ const router = createRouter({
       component: () => import("../views/About.vue"),
     },
     {
-      path: "/floorIntroduction", //自己取之後要連的路徑名稱
+      path: "/floor_introduction", //自己取之後要連的路徑名稱
       name: "FloorIntroduction", //自己取
       component: () => import("../views/FloorIntroduction.vue"), //路徑連到剛剛建的vue檔
     },
     {
-      path: "parkActivity", //自己取之後要連的路徑名稱
+      path: "/park_activity", //自己取之後要連的路徑名稱
       name: "ParkActivity", //自己取
       component: () => import("../views/ParkActivity.vue"), //路徑連到剛剛建的vue檔
     },
@@ -83,25 +85,28 @@ const router = createRouter({
       path: "/news",
       name: "News",
       component: () => import("../views/News.vue"),
+      children:[
+        {
+          path: "news_page",
+          name: "NewsPage",
+          component: () => import("../views/news/NewsPage.vue"),
+        },
+        ]
     },
-    {
-      path: "/newspage",
-      name: "NewsPage",
-      component: () => import("../views/NewsPage.vue"),
-    },
+   
     {
       path: "/ticket", //自己取之後連的路徑名稱
       name: "ticket", //自己取
       component: () => import("../views/Ticket.vue"), //路徑連到剛剛建的vue檔
       //children是這個頁面的子頁面
-      // children:[
-      //   {
-      //     path: 'yearpass',
-      //     name: 'yearpass',
-      //     component: ()=> import("../views/ticket/yearpass.vue")
+      children:[
+        {
+          path: 'yearpass',
+          name: 'yearpass',
+          component: ()=> import("../views/ticket/Yearpass.vue")
 
-      //   }
-      // ]
+        }
+      ]
     },
     // {
     //   path: '/:id',
