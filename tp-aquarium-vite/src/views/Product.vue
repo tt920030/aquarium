eec7b3294b4b1e669e69ac402f1ddc273d69a793
AshlyKuo <ashlykuo1998@gmail.com>
2023-05-20 00:22:45 +0800

    Add Stores,Product and Customer Service function

--- tp-aquarium-vite/src/views/Product.vue
+++ tp-aquarium-vite/src/views/Product.vue
@@ -7,22 +7,26 @@
         <main>
             <div class="wrapper navigation">
                 <p>網路商城 > </p>
-                <a href=""><p class="product_name"> 企鵝娃娃</p></a>
+                <a href=""><p class="product_name"> {{ data.name }}</p></a>
             </div>
             <div class="wrapper content">
                 <div class="photo">
-                    <img src="../img/product_penguinDoll.jpg" alt="">
+                    <img :src="imgUrl" alt="">
                     <div>
-                        <img src="../img/product_penguinDoll.jpg" alt="">
-                        <img src="../img/product_penguinDoll.jpg" alt="">
-                        <img src="../img/product_penguinDoll.jpg" alt="">
+                        <img 
+                        v-for="(img, index) in data.imgs" 
+                        :src="img" 
+                        :key="index"
+                        :class="{imgOn: currentImg == index}" 
+                        alt="" 
+                        @click="imgUrl = img; currentImg = index">
                     </div>
                 </div>
                 <div class="rightside">
                     <div class="product_info">
-                        <h3 class="product_name">企鵝娃娃</h3>
-                        <h4 class="product_price">NTD $500</h4>
-                        <p class="product_intro">企鵝絨毛娃娃的外觀可愛，經典的設計包括黑白相間的身體和圓滾滾的身材，令人難以抗拒。面料使用高品質的絨毛製成，手感柔軟舒適。</p>    
+                        <h3 class="product_name">{{ data.name }}</h3>
+                        <h4 class="product_price">NTD ${{ data.price }}</h4>
+                        <p class="product_intro">{{ data.intro }}</p>    
                     </div>
                     <div class="numberBtn">
                         <!-- 增加按鈕 -->
@@ -38,19 +42,18 @@
                 </div>
             </div>
             <div class="wrapper precaution">
-                <div>
-                    <h4 v-for="tab in tabs">{{ tab.id }}</h4>
-                    <h4 class="freeDelivery">免運標準</h4>
-                    <h4 class="payment ">付款方式</h4>
-                    <h4 class="">出貨時間</h4>
-                    <h4 class="refund  btnOn">退換貨須知</h4>
+                <!-- <button @click="clickHandle">借看一下</button> -->
+                <div class="precautionTitle">
+                    <button
+                    type="button" 
+                    v-for="tab in tabs" 
+                    :key="tab.id"
+                    @click="currentTab = tab.id ;clickHandle() " 
+                    :class="{btnOn: currentTab == tab.id}">{{ tab.title }}</button>
                     <span></span>
                 </div>
                 <div>
-                    <p class="">滿NTD 1000即可享有免運</p>
-                    <p class="">信用卡<br>第三方支付</p>
-                    <p class="">將於付款完成後，5-7個工作天寄出</p>
-                    <p class="contentShow">商品到貨享十天猶豫期之權益（注意！猶豫期非試用期），辦理退貨商品必須是全新狀態且包裝完整，商品一經拆封，等同商品價值已受損，僅能以福利品出售，若需退換貨，我方須收取價值損失之費用(回復原狀、整新費)，請先確認商品正確、外觀可接受，再行使用，以免影響您的權利。</p>
+                    <p>{{ contentShow }}</p>
                 </div>
             </div>
 
@@ -68,25 +71,56 @@
 
     // 數量
     let counter = ref(1);
-    // 購買須知
+    // 預設tab
+    let currentTab = ref(0)
+    // 預設顯示內容
+    let contentShow = ref('滿NTD 1000即可享有免運')
+    // 大圖src
+    let imgUrl = ref('src/img/product_1_1.jpg')
+    // 預設大圖
+    let currentImg = ref(0)
+
+    // 購買須知內容
     const tabs = reactive([
         {
-            id: '免運標準',
+            id: 0,
+            title: '免運標準',
             content: '滿NTD 1000即可享有免運'
         },
         {
-            id:'付款方式',
+            id: 1,
+            title:'付款方式',
             content: '信用卡/第三方支付'
         },
         {
-            id: '出貨時間',
+            id: 2,
+            title: '出貨時間',
             content: '將於付款完成後，5-7個工作天寄出'
         },
         {
-            id:'退換貨須知',
+            id: 3,
+            title: '退換貨須知',
             content: '商品到貨享十天猶豫期之權益（注意！猶豫期非試用期），辦理退貨商品必須是全新狀態且包裝完整，商品一經拆封，等同商品價值已受損，僅能以福利品出售，若需退換貨，我方須收取價值損失之費用(回復原狀、整新費)，請先確認商品正確、外觀可接受，再行使用，以免影響您的權利。'   
         }
     ])
+
+    //商品資訊
+    const data = reactive({
+        name: '企鵝娃娃',
+        price: 500,
+        intro: '企鵝絨毛娃娃的外觀可愛，經典的設計包括黑白相間的身體和圓滾滾的身材，令人難以抗拒。面料使用高品質的絨毛製成，手感柔軟舒適。',
+        imgs: [
+        'src/img/product_1_1.jpg',
+        'src/img/product_1_2.jpg',
+        'src/img/product_1_3.jpg'
+        ]
+    })
+
+    //切換顯示內容
+    function clickHandle(){
+        contentShow.value = tabs[currentTab.value].content;
+        console.log(typeof (data.imgs[0].src))
+    }
 
     // 增加數量
     function addCounter(){
@@ -100,6 +134,8 @@
             counter.value--;
         }
     }
+
+
 
 </script>
 <style lang="scss" scoped>
