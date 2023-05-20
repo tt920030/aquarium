cb7f05f4eec0e283357e5f89778851563d30d8ec
anniesuzzz <annie10030824@gmail,com>
2023-05-20 13:45:30 +0800

    no message

---
+++ tp-aquarium-vite/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView, useRouter } from 'vue-router';
+
+
+// const router = useRouter();
+
+
+</script>
+
+<template>
+
+  <!-- <router-link to="/"></router-link> -->
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+
+  @import "@/assets/sass/style.scss";
+  
+</style>
